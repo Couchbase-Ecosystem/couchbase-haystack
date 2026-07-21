cff897a8cb9ff02d4f666eeec0ed8566454aa401
Viraj Agarwal <virajagarwal15@gmail.com>
2026-07-21 14:13:34 +0530

    Update: reo telemetry activity type

--- src/couchbase_haystack/reo_telemetry.py
+++ src/couchbase_haystack/reo_telemetry.py
@@ -12,7 +12,7 @@ def _send_telemetry() -> None:
             package_name="couchbase-haystack",
             package_version=_pkg_version,
         )
-        _logger.log_event()
+        _logger.log_event({"activity_type": "package_import"})
     except Exception:
         pass
 
